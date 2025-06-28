31b56c51977feb22e426584f24fe2b793e0e391d
VishnuKanna <vishnumas111@gmail.com>
2025-06-28 08:04:13 +0530

    Changes v2

--- forms/contact.php
+++ forms/contact.php
@@ -1,15 +1,14 @@
-header("Access-Control-Allow-Origin: *");
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $name = htmlspecialchars($_POST['name']);
   $email = htmlspecialchars($_POST['email']);
   $subject = htmlspecialchars($_POST['subject']);
   $message = htmlspecialchars($_POST['message']);
 
-  // Where to send the email
-  $to = "divyaselvakumar29@gmail.com"; 
+  $to = "divyaselvakumar29@gmail.com";
   $headers = "From: $email\r\n";
   $headers .= "Reply-To: $email\r\n";
   $headers .= "Content-type: text/plain; charset=UTF-8\r\n";
@@ -17,10 +16,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $fullMessage = "Name: $name\nEmail: $email\nSubject: $subject\n\nMessage:\n$message";
 
   if (mail($to, $subject, $fullMessage, $headers)) {
-    echo "Your message has been sent. Thank you!";
+    echo json_encode(['success' => true, 'message' => 'Your message has been sent. Thank you!']);
   } else {
     http_response_code(500);
-    echo "Something went wrong. Please try again later.";
+    echo json_encode(['success' => false, 'message' => 'Something went wrong. Please try again later.']);
   }
+} else {
+  http_response_code(405);
+  echo json_encode(['success' => false, 'message' => 'Method Not Allowed']);
 }
 ?>
